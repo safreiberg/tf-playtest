8c8eb8c4ea14368df39b2495368fbeeb4f1fb119
Stephen Freiberg <safreiberg@gmail.com>
2020-08-04 01:08:16 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tf-playtest
+
+Following tutorial here: https://www.tensorflow.org/hub/tutorials/object_detection
---
+++ test.py
@@ -0,0 +1,48 @@
+# For running inference on the TF-Hub module.
+import tensorflow as tf
+
+import tensorflow_hub as hub
+
+# For measuring the inference time.
+import time
+
+from render_utils import download_and_resize_image
+from render_utils import display_image
+from render_utils import draw_boxes
+
+# Print Tensorflow version
+print(tf.__version__)
+
+# By Heiko Gorski, Source: https://commons.wikimedia.org/wiki/File:Naxos_Taverna.jpg
+image_url = "https://upload.wikimedia.org/wikipedia/commons/6/60/Naxos_Taverna.jpg"  
+downloaded_image_path = download_and_resize_image(image_url, 1280, 856, True)
+
+module_handle = "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1" 
+
+detector = hub.load(module_handle).signatures['default']
+
+def load_img(path):
+  img = tf.io.read_file(path)
+  img = tf.image.decode_jpeg(img, channels=3)
+  return img
+
+def run_detector(detector, path):
+  img = load_img(path)
+
+  converted_img  = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
+  start_time = time.time()
+  result = detector(converted_img)
+  end_time = time.time()
+
+  result = {key:value.numpy() for key,value in result.items()}
+
+  print("Found %d objects." % len(result["detection_scores"]))
+  print("Inference time: ", end_time-start_time)
+
+  image_with_boxes = draw_boxes(
+      img.numpy(), result["detection_boxes"],
+      result["detection_class_entities"], result["detection_scores"])
+
+  display_image(image_with_boxes)
+
+run_detector(detector, downloaded_image_path)
